8e72b7994209ded86ff04cb594ef7aee95797c7a
Sam Marlow <sam.marlow9@gmail.com>
2021-12-02 13:49:33 +0000

    Day 2

--- AdventofCode/Program.cs
+++ AdventofCode/Program.cs
@@ -9,11 +9,16 @@ namespace AdventofCode
             string text = System.IO.File.ReadAllText(@"C:\Users\sam_b\Desktop\AdventofCode\Day1\day_1_input.txt");
             string[] values = text.Split('\n');
             int iPos = 0;
-            int iIncrease = 0;
+
             int iDay1Part1 = 0;
             int iDay1Part2 = 0;
+            int iDay2Part1 = 0;
+            int iDay2Part2 = 0;
+
             int iSum = 0;
+
             // Day 1 - Part 1
+            /*
             do
             {
                 if (iPos > 0)
@@ -31,6 +36,7 @@ namespace AdventofCode
             int iOldSum = 0;
 
             iPos = 0;
+
 
             // Day 1 - Part 2
             do
@@ -55,9 +61,77 @@ namespace AdventofCode
                 iPos++;
 
             } while (values[iPos] != "" && values[iPos + 1] != "" &&  values[iPos + 2] != "");
+            */
+            // Day 2 Part 1
+            text = System.IO.File.ReadAllText(@"C:\Users\sam_b\Desktop\AdventofCode\Day2\day_2_input.txt");
+            values = text.Split('\n');
+            
+            iPos = 0;
+
+            int iHoriz = 0;
+            int iDepth = 0;
+            string[] cInstruction;
+
+            do
+            {
+                cInstruction = values[iPos].Split(" ");
+
+                if (cInstruction[0] =="up")
+                {
+                    iDepth = iDepth - int.Parse(cInstruction[1]);
+                }
+                else if (cInstruction[0] == "down")
+                {
+                    iDepth = iDepth + int.Parse(cInstruction[1]);
+                }
+                else if (cInstruction[0] == "forward")
+                {
+                    iHoriz = iHoriz + int.Parse(cInstruction[1]);
+                }
+
+                iPos++;
+
+            } while (values[iPos] != "");
+
+            iDay2Part1 = iHoriz * iDepth;
+
+            // Day 2 Part 2
+            iPos = 0;
+            iHoriz = 0;
+            iDepth = 0;
+            int iAim = 0;
+            do
+            {
+                cInstruction = values[iPos].Split(" ");
+
+                if (cInstruction[0] == "up")
+                {
+                   // iDepth = iDepth - int.Parse(cInstruction[1]);
+                    iAim   = iAim - int.Parse(cInstruction[1]);
+                }
+                else if (cInstruction[0] == "down")
+                {
+                    //iDepth = iDepth + int.Parse(cInstruction[1]);
+                    iAim = iAim + int.Parse(cInstruction[1]);
+                }
+                else if (cInstruction[0] == "forward")
+                {
+                    iHoriz = iHoriz + int.Parse(cInstruction[1]);
+                    iDepth = iDepth + (iAim * int.Parse(cInstruction[1]));
+                }
+
+                iPos++;
+
+            } while (values[iPos] != "");
+
+            iDay2Part2 = iHoriz * iDepth;
 
             System.Console.WriteLine($"Day 1 - Part 1: {iDay1Part1}");
             System.Console.WriteLine($"Day 1 - Part 2: {iDay1Part2}");
+            System.Console.WriteLine($"Day 1 - Part 2: {iDay2Part1}");
+            System.Console.WriteLine($"Day 2 - Part 2: {iDay2Part2}");
+
+
 
             System.Console.ReadKey();
         }
